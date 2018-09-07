d8c54d70b04d2b4524b93b20d7b1612c1a2debad
Turiano Romero <romero.guzman18@gmail.com>
2018-09-07 12:48:57 -0500

    captutrando el valor del input del nav :blush:

--- src/app/github.js
+++ src/app/github.js
@@ -5,7 +5,14 @@ class Github{
 		this.client_secret = clientSecret
 	}
 
-	fetchUser(user){
-	   fetch(`http://api.github.com/users/?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+	async fetchUser(user){
+	   const userRequest = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&
+	   	client_secret=${this.client_secret}`);
+
+	   const userData = await userRequest.json
+	   
+	   console.log(userData);
 	}
 }
+
+module.exports = Github
--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,15 @@
 const ui = require('./ui');
-const github = require('./github');
+const Github = require('./github');
 
 const { client_id, client_secret } = require('./config.json');
+
+const github = new Github(client_id, client_secret);
+
+const userForm = document.getElementById('userForm');
+
+userForm.addEventListener('submit', (e) => {
+	
+	const textSearch = document.getElementById('textSearch').value; //captura lo que escriba en input
+ 	console.log(textSearch);
+	e.preventDefault();
+});
