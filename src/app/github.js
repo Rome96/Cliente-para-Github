d530223117f547eb3d5d0466a62aa432d8abd684
Turiano Romero <romero.guzman18@gmail.com>
2018-09-07 11:41:39 -0500

    realizando peticiones a github :sunglasses:

--- src/app/github.js
+++ src/app/github.js
@@ -1,1 +1,11 @@
-github.js
+class Github{
+
+	constructor(clientId, clientSecret){
+		this.client_id = clientId;
+		this.client_secret = clientSecret
+	}
+
+	fetchUser(user){
+	   fetch(`http://api.github.com/users/?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+	}
+}
